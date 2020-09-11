b7abdbf5b3c4fe3e0d0b015c9d976db72aef696a
Daniel Crewdson <daniel.crewdson@steadfastcollective.com>
2020-09-11 13:09:31 +0100

    init build

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col justify-between px-10">
+      <div class="header mb-10">
+        <h1 class="mt-6 text-white text-4xl">Toby Dart</h1>
+      </div>
+      <div class="content mb-10">
+        <div style="padding:56.25% 0 0 0;position:relative;">
+            <iframe src="https://player.vimeo.com/video/373406844?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+        </div>
+      </div>
+      <div class="footer flex justify-center items-center">
+        <a class=" text-green-500 hover:text-indigo-500" href="https://twitter.com/yhchkn_">twitter</a>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
